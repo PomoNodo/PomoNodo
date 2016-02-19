19587398030b625c68243f92a70d5681d504ed14
alexwander <ryndyuk.alexander@gmail.com>
2016-02-19 18:58:21 +0200

    h

--- PomodoApp.js
+++ PomodoApp.js
@@ -1,32 +1,17 @@
-/**
+﻿/**
  * Created by alexandr on 12.02.2016.
  */
 
 "use strict";
-function PomodoApp() {
-    var version = "v1.0";
 
+function PomodoApp()
+{
+    var version = "v1.2";
 
-    $(function () {
-        window.app = new PomodoApp();
-        window.app.start();
-    });
-
-    this.start = function ()
+    function setStatus(message)
     {
-        $("#new-task-name").keypress(function (e) {
-                if (e.which == 13) //enter key
-                {
-                    addTask();
-                    return false;
-                }
-            })
-            .focus();
-
-            $("#app>header").append(version);
-            setStatus("ready");
-    };
-
+        $("#app>footer").text(message);
+    }
 
     function addTask()
     {
@@ -34,16 +19,67 @@ function PomodoApp() {
         if (taskName)
         {
             addTaskElement(taskName);
-            //reset text field
+            // Reset the field
             $("#new-task-name").val("").focus();
         }
     }
 
     function addTaskElement(taskName)
     {
-        var $task = $("<li></li>");
-        $task.text(taskName);
+        var $task = $("#task-template .task").clone();
+        $("span.task-name", $task).text(taskName);
+
         $("#task-list").append($task);
 
+        // Button events
+        $("button.delete", $task).click(function() { $task.remove(); });
+        $("button.move-up", $task).click(function() { $task.insertBefore($task.prev()); });
+        $("button.move-down", $task).click(function() { $task.insertAfter($task.next()); });
+
+        // Task name events
+        $("span.task-name", $task).click(function() { onEditTaskName($(this)); });
+        $("input.task-name", $task).change(function() { onChangeTaskName($(this)); })
+            .blur(function() { $(this).hide().siblings("span.task-name").show(); });
     }
+
+    function onEditTaskName($span)
+    {
+        $span.hide()
+            .siblings("input.task-name").val($span.text()).show().focus();
+    }
+
+    function onChangeTaskName($input)
+    {
+        $input.hide();
+        var $span = $input.siblings("span.task-name");
+        if ($input.val())
+        {
+            $span.text($input.val());
+        }
+        $span.show();
+    }
+
+    this.start = function()
+    {
+        $("#new-task-name").keypress(function(e)
+            {
+                if (e.which == 13) // Enter key
+                {
+                    addTask();
+                    return false;
+                }
+            })
+            .focus();
+
+        $("#app>header").append(version);
+        setStatus("ready");
+    };
 }
+
+
+
+$(function()
+{
+    window.app = new PomodoApp();
+    window.app.start();
+});
