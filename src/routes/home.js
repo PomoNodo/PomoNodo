df2b9e47bc9a144e2b5a81249d4240b210d3c933
alexwander <ryndyuk.alexander@gmail.com>
2016-10-14 20:40:50 +0300

    add home routes

---
+++ src/routes/home.js
@@ -0,0 +1,30 @@
+const getRepos = require('../lib/getRepos.js');
+
+module.exports = {
+  path: '/',
+  method: 'GET',
+  config: {
+    auth: {
+      mode: 'try', // try to go to page even if the strategy is not loaded
+      strategy: 'session'
+    }
+  },
+  handler: (req, rep) => {
+    const homeObj = {
+      title: 'PomoNodo - welcome!',
+      loggedIn: req.auth.isAuthenticated
+    };
+
+    if (req.auth.isAuthenticated) {
+      // add cookie info here!
+      getRepos(req.auth.credentials, (err, res, body) => {
+        if (err) { throw err; }
+        const currentUser = { userName: req.auth.credentials.userName };
+        const repos = { repos: JSON.parse(body) };
+        rep.view('home', Object.assign(homeObj, repos, currentUser));
+      });
+    } else {
+      rep.view('home', homeObj);
+    }
+  }
+};
