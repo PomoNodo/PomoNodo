9ddd778b79ab521d110f555b116f504569ba784e
alexwander <ryndyuk.alexander@gmail.com>
2016-10-15 19:42:40 +0300

    add index pages

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+module.exports = [
+  require('./home.js'),
+  require('./login.js'),
+  require('./logout.js'),
+  require('./welcome.js'),
+  require('./repo.js'),
+  require('./static.js'),
+  require('./issue.js'),
+  require('./assign.js'),
+  require('./complete.js'),
+  require('./abandon.js')
+];
