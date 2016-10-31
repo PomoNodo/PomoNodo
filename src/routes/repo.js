bb6cdb9dd133c06065a2326dd8fd7769701d7cac
alexwander <ryndyuk.alexander@gmail.com>
2016-10-31 20:04:11 +0200

    add GET repo

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -1,4 +1,19 @@
 const getIssues = require('../lib/getIssues');
 
+module.exports = {
+  method: 'GET',
+  path: '/repo',
+  config: {
+    auth: {
+      mode: 'try',
+      strategy: 'session'
+    }
+  },
+  handler: (req, rep) => {
+    const homeObj = {
+      title: 'PomoNodo - view issues!',
+      loggedIn: req.auth.isAuthenticated
+    };
 
+   
 };
