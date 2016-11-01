8d633a80ec80841c9bc506f78bca15962b6c165d
alexwander <ryndyuk.alexander@gmail.com>
2016-11-01 21:56:59 +0200

    add json parse to get repo

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -15,5 +15,17 @@ module.exports = {
       loggedIn: req.auth.isAuthenticated
     };
 
-   
+    const userSelection = {
+      repoOwner: req.query.repo.split(' ')[0],
+      repoName: req.query.repo.split(' ')[1],
+      repoUrl: function () { return `https://github.com/${this.repoOwner}/${this.repoName}/issues`; }
+    };
+    if (req.auth.isAuthenticated) {
+      getIssues(Object.assign(req.auth.credentials, userSelection), (err, res, body) => {
+        if (err) throw err;
+        const issues = { issues: JSON.parse(body) };
+        rep.view('issues', Object.assign(homeObj, req.auth.credentials, userSelection, issues));
+      });
+    }
+  }
 };
