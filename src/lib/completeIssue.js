4ebecf2cf4a15898d547354bd45295b6fdedfd8d
alexwander <ryndyuk.alexander@gmail.com>
2016-11-08 21:26:12 +0200

    req compl issue

--- src/lib/completeIssue.js
+++ src/lib/completeIssue.js
@@ -7,5 +7,17 @@ module.exports = (options, cb) => {
   };
   const labelUrl = options.issueUrl + '/labels';
   const newLabelPayload = [ 'Ready for Review' ];
-  
+  Request.delete({
+    url: labelUrl + '/In%20Progress',
+    headers: headers
+  }, (err, res, body) => {
+    if (err) cb(err);
+    else {
+      Request.post({
+        url: labelUrl,
+        headers: headers,
+        json: newLabelPayload
+      }, cb);
+    }
+  });
 };
