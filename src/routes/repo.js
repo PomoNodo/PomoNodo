7c1ef716cb1e62b1a44ef434ee107778aacc2712
alexwander <ryndyuk.alexander@gmail.com>
2016-12-18 20:57:54 +0200

    add module POST req handler

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -77,4 +77,13 @@ module.exports = {
       strategy: 'session'
     }
   },
-
+      handler: (req, rep) => {
+      const issueDetails = req.payload;
+if (req.auth.isAuthenticated) {
+  completeIssue(Object.assign(req.auth.credentials, issueDetails), (err, res, body) => {
+    if (err) throw err;
+  rep.redirect('/');
+});
+}
+}
+};
