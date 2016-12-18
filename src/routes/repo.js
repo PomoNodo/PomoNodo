abfed443362123b91e22eab450b407a73ab283d6
alexwander <ryndyuk.alexander@gmail.com>
2016-12-18 20:58:21 +0200

    add module POST req handler

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -69,21 +69,23 @@ if (req.auth.isAuthenticated) {
 
 
 module.exports = {
-  method: 'POST',
-  path: '/complete',
-  config: {
-    auth: {
-      mode: 'try',
-      strategy: 'session'
-    }
-  },
-      handler: (req, rep) => {
+      method: 'POST',
+      path: '/complete',
+      config: {
+        auth: {
+          mode: 'try',
+          strategy: 'session'
+        }
+      },
+      handler: (req, rep) = > {
       const issueDetails = req.payload;
 if (req.auth.isAuthenticated) {
-  completeIssue(Object.assign(req.auth.credentials, issueDetails), (err, res, body) => {
+  completeIssue(Object.assign(req.auth.credentials, issueDetails), (err, res, body) = > {
     if (err) throw err;
   rep.redirect('/');
-});
+})
+  ;
 }
 }
-};
+}
+;
