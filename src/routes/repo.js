49978832b0298e41a3dc2e32d7d4345202aab8e9
alexwander <ryndyuk.alexander@gmail.com>
2017-01-02 23:23:38 +0200

    add assign user module handler

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -102,3 +102,13 @@ module.exports = {
       strategy: 'session'
     }
   },
+      handler: (req, rep) => {
+      const body = JSON.parse(req.payload);
+const issueDetails = { issueUrl: body.issueUrl };
+if (req.auth.isAuthenticated) {
+  assignUser(Object.assign(req.auth.credentials, issueDetails), (err, res, body) => {
+    if (err) throw err;
+});
+}
+}
+};
