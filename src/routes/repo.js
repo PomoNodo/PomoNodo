9793ca5ece669417e7693cec5403ca040a3dd3b1
alexwander <ryndyuk.alexander@gmail.com>
2017-02-14 21:15:27 +0200

    add ab issue module

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -123,3 +123,14 @@ module.exports = {
       strategy: 'session'
     }
   },
+
+      handler: (req, rep) => {
+      const issueDetails = req.payload;
+if (req.auth.isAuthenticated) {
+  abandonIssue(Object.assign(req.auth.credentials, issueDetails), (err, res, body) => {
+    if (err) throw err;
+  rep.redirect('/');
+});
+}
+}
+};
