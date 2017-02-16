b0c644b739a02cc117825f5e4707349fab7e9bac
alexwander <ryndyuk.alexander@gmail.com>
2017-02-16 20:10:19 +0200

    add ab issue module

--- src/routes/repo.js
+++ src/routes/repo.js
@@ -115,22 +115,26 @@ if (req.auth.isAuthenticated) {
 
 
 module.exports = {
-  method: 'POST',
-  path: '/abandon',
-  config: {
-    auth: {
-      mode: 'try',
-      strategy: 'session'
-    }
-  },
+      method: 'POST',
+      path: '/abandon',
+      config: {
+        auth: {
+          mode: 'try',
+          strategy: 'session'
+        }
+      },
 
-      handler: (req, rep) => {
+      handler: (req, rep) = > {
       const issueDetails = req.payload;
 if (req.auth.isAuthenticated) {
-  abandonIssue(Object.assign(req.auth.credentials, issueDetails), (err, res, body) => {
+  abandonIssue(Object.assign(req.auth.credentials, issueDetails), (err, res, body) = > {
     if (err) throw err;
   rep.redirect('/');
-});
+})
+  ;
 }
 }
-};
+}
+;
+
+
