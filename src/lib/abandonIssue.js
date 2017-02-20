330796be139203c68339fa01cd8738f1b9ddba38
alexwander <ryndyuk.alexander@gmail.com>
2017-02-20 21:25:08 +0200

    add ab issue API del req

--- src/lib/abandonIssue.js
+++ src/lib/abandonIssue.js
@@ -9,5 +9,17 @@ module.exports = (options, cb) => {
   const labelUrl = options.issueUrl + '/labels/In%20Progress';
   const assignPayload = { 'assignees': [ options.userName ] };
 
-  
+  Request.delete({
+    url: assignUrl,
+    headers: headers,
+    json: assignPayload
+  }, (err, res, body) => {
+    if (err) cb(err);
+    else {
+      Request.delete({
+        url: labelUrl,
+        headers: headers
+      }, cb);
+    }
+  });
 };
